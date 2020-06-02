f828d739b87acaf2f8d66700f10d44573a2189a8
尤羿 <53115957+Clearlovesixteen@users.noreply.github.com>
2020-06-02 15:47:59 +0800

    22

--- lodash/clearlovesixteen-lodash.js
+++ lodash/clearlovesixteen-lodash.js
@@ -1,5 +1,21 @@
-var Clearlovesixteen = {
-    .chunk:function (){
-        
-    }
+var clearlovesixteen = {
+    .chunk:function ( array ,  size ){
+        var len = array.length;
+        var arr = [] ;
+        var arr1 = [] ;
+        var count = 0 ;
+        for(var i = 0  ; i < len ; i++){
+            if(count == size){
+                arr.push(arr1)
+                count = 0;
+                arr1 = []
+            }else{
+                arr1.push(array[i])
+                count++
+            }
+        }
+        return arr
+    },
+    
+    
 }
