de991db709df33e4049b9a8fa68473c321946a91
尤羿 <53115957+Clearlovesixteen@users.noreply.github.com>
2020-06-05 15:38:57 +0800

    2223

--- lodash/clearlovesixteen-lodash.js
+++ lodash/clearlovesixteen-lodash.js
@@ -495,7 +495,7 @@ var clearlovesixteen = {
          }
      },
      isNaN:function(value){
-         if( toString(value) == "[object Undefined]" && typeof value == "number"){
+         if( toString(value) == "[object Undefined]" && typeof value == "number" || "object"){
              return true
          }
          return false
